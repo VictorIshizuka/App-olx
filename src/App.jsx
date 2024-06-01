ed9a579664507cfaeb8e4544268a36af5714bffa
Victor Rosario Ishizuka <victorishizuka@outlook.com>
2024-06-01 18:37:39 -0300

    Projeto inicial ecommerce estudo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import { IsRoutes } from "./routes";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <IsRoutes />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/routes/index.jsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "../modules";
+export const IsRoutes = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="" element={<Home />} />
+      </Routes>
+    </>
+  );
+};
